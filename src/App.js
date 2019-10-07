abf07463dec03a5441e0333dd733c58587b51ace
roksard <roksard@gmail.com>
2019-10-07 21:11:36 +0500

    Promises basics

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component} from "react";
 import "./App.css";
 
 class App extends Component{
+    componentDidMount() {
+        const resolve1 = () => console.log("resolve");
+        const reject1 = () => console.log("reject");
+        new Promise((resolve, reject) => {
+            if (1===2) {
+                resolve();
+            } else {
+                reject();
+            }
+        })
+            .then(resolve1, reject1)
+            .catch(() => console.log("catch"));
+    }
+
     render() {
         return (
             <div className="App">
-                <h1> Hello, World! </h1>
+                <h1> Hello, World1! </h1>
             </div>
         );
     }
